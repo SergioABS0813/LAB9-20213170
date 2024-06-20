872dc25709fb2428db00069fd2ec9504f3375df7
SergioABS0813 <sergioandre.bustamante@gmail.com>
2024-06-19 19:12:40 -0500

    primer commit

---
+++ LAB9-20213170/src/main/java/com/example/lab920213170/Lab920213170Application.java
@@ -0,0 +1,13 @@
+package com.example.lab920213170;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab920213170Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab920213170Application.class, args);
+    }
+
+}
---
+++ LAB9-20213170/src/test/java/com/example/lab920213170/Lab920213170ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab920213170;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab920213170ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
